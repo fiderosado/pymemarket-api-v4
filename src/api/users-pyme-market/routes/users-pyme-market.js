a4492a3d80b50166808ea689078ab904b8ecbb88
fiderosado <fiderosado@gmail.com>
2022-11-20 15:17:53 -0500

    Fix Files

--- src/api/users-pyme-market/routes/users-pyme-market.js
+++ src/api/users-pyme-market/routes/users-pyme-market.js
@@ -1,6 +1,9 @@
 'use strict';
-/*
+
+/**
  * users-pyme-market router
  */
+
 const { createCoreRouter } = require('@strapi/strapi').factories;
+
 module.exports = createCoreRouter('api::users-pyme-market.users-pyme-market');
